a526c54376888f325b4869bb803b1f7573851d70
gmnayem631 <gmnayem631@gmail.com>
2026-03-02 23:23:07 +0600

    installed tailwindcss and daisyUI to the project

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>BPL Dream</h1>
+      <h1 className="text-red-400 font-bold text-6xl">BPL Dream</h1>
+      <button className="btn btn-soft btn-accent">Accent</button>
     </>
   );
 }
