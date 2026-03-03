215139fbd36cd24d73e7b3030431e707f2617ff4
gmnayem631 <gmnayem631@gmail.com>
2026-03-03 11:22:45 +0600

    added the navigatoin bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import "./App.css";
+import logo from "./assets/logo.png";
+import dollarImg from "./assets/currency.png";
 
 function App() {
   return (
     <>
-      <h1 className="text-red-400 font-bold text-6xl">BPL Dream</h1>
-      <button className="btn btn-soft btn-accent">Accent</button>
+      <div className="navbar bg-base-100 max-w-7xl mx-auto">
+        <div className="flex-1">
+          <a className="text-xl">
+            <img className="w-15 h-15 cursor-pointer" src={logo} alt="" />
+          </a>
+        </div>
+        <div className="flex items-center justify-center gap-2">
+          <span>6000000000</span>
+          <span> Coin</span>
+          <img src={dollarImg} alt="" />
+        </div>
+      </div>
     </>
   );
 }
